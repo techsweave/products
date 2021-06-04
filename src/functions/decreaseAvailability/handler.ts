a8df8d42d89f1622d163298f2dbbaf83949c23c8
samupd2013 <samupd2013@gmail.com>
2021-06-04 10:01:21 +0200

    fix

--- src/functions/decreaseAvailability/handler.ts
+++ src/functions/decreaseAvailability/handler.ts
@@ -7,18 +7,16 @@ import schema from './schema';
  * Remember: event.body type is the type of the instantiation of ValidatedEventAPIGatewayProxyEvent
  * In this case event.body type is 'void' -> we have no body!
 */
-const getProductHandler: ValidatedEventSQSEvent<typeof schema> = async (event) => {
+const decreaseAvailabilityHandler: ValidatedEventSQSEvent<typeof schema> = async (event) => {
 
     try {
         await decreaseAvailability(event.Records[0].messageAttributes.id.stringValue, parseInt(event.Records[0].messageAttributes.availabilityQta.stringValue));
     }
     catch (error) {
         //console.log prints to AWS CloudWatch
-        console.log('EVENT:');
-        console.log(event);
-        console.log('ERROR:');
-        console.log(error);
+        console.log('EVENT:\n' + event + '\nERROR\n:' + error);
     }
+
 };
 
-export const main = middyfy(getProductHandler);
+export const main = middyfy(decreaseAvailabilityHandler);
