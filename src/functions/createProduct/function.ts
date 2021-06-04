3b14d713d0a584a0fc5ea3d9bf22301bbe0eecd0
samupd2013 <samupd2013@gmail.com>
2021-06-04 17:25:29 +0200

    added SKU check

--- src/functions/createProduct/function.ts
+++ src/functions/createProduct/function.ts
@@ -3,6 +3,11 @@ import Product from '@dbModel/tables/product';
 
 
 const createProduct = async (item: Product): Promise<Product> => {
+    if (await dbContext.query(Product, { SKU: item.SKU }).count == 0)
+        throw {
+            name: 'DuplicateSKU',
+            message: 'Can\'t create a product whit already existing SKU'
+        };
     return dbContext.put(item);
 };
 
